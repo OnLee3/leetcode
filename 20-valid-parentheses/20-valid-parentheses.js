fd184a4e9f7c463a485f83829013b020cc564182
이재석 <83897458+OnLee3@users.noreply.github.com>
2022-04-26 13:50:46 +0900

    Time: 69 ms (74.09%), Space: 41.9 MB (91.30%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -5,21 +5,18 @@
 var isValid = function(s) {
     const stack = [];
     const open = ["(", "[", "{"]
+    const close = {
+        "}": "{",
+        "]": "[",
+        ")": "("
+    }
     
     for (let i=0; i<s.length; i++){
         if (open.includes(s[i])) stack.push(s[i])
-        else {
-            if (s[i] === "}" && stack[stack.length-1] === "{") {
-                stack.pop()
-            }
-            else if (s[i] === "]" && stack[stack.length-1] === "[") {
-                stack.pop()
-            }
-            else if (s[i] === ")" && stack[stack.length-1] === "(") {
-                stack.pop()
-            }
-            else return false;
+        else if (close[s[i]] === stack[stack.length-1]) {
+            stack.pop()
         }
+        else return false;
     }
     if (stack.length > 0) return false;
     return true;
